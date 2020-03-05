2c4ef66872a5ce8a2865cf80269d9b7bdc0411c3
Sven Schroeder <schuen@gmx.de>
2020-03-05 17:15:05 +0100

    finished predicate

--- src/main/java/functionalinterface/MyPredicate.java
+++ src/main/java/functionalinterface/MyPredicate.java
@@ -1,14 +1,31 @@
 package functionalinterface;
 
+import java.util.function.Predicate;
+
 public class MyPredicate {
 	
 	public static void main(String[] args) {
-		System.out.println("07000000000");
-		System.out.println("08000asd0000");
+		System.out.println(isPhoneNumberValid("07000000000"));
+		System.out.println(isPhoneNumberValid("08000asd0000"));
+		
+		boolean isPhoneNrValid = phoneNr.test("07000000000");
+		System.out.println(isPhoneNrValid);
+		
+//		combining predicates: Both predicates do have to have the same generic types bzw. super types
+		System.out.println("combining");
+		System.out.println(phoneNr.and(containsNr).test("07000000000"));
+		System.out.println(phoneNr.and(containsNr).test("08000000000"));
+//		with or instead of and
+		System.out.println(phoneNr.or(containsNr).test("07000000000"));
 	}
 	
 	static boolean isPhoneNumberValid(String number) {
 		return number.startsWith("07")&& number.length() == 11;
 	}
+	
+//	same with predicate
+	static Predicate<String> phoneNr = nr -> nr.startsWith("07")&& nr.length()==11;
+	
+	static Predicate<String> containsNr = number -> number.contains("09");
 
 }
